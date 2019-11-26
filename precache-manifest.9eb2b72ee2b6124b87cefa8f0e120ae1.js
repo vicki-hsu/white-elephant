6219a21dd0727c14cbaea7fa23716123cbe605be
Timothy M. Duly <timduly4@gmail.com>
2019-11-25 20:40:00 -0700

    Updates

---
+++ precache-manifest.9eb2b72ee2b6124b87cefa8f0e120ae1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7f5bb5cdb081fc52cd38147217ee97",
+    "url": "/white-elephant/index.html"
+  },
+  {
+    "revision": "b9e097888d57571795f3",
+    "url": "/white-elephant/static/css/2.7bbceaae.chunk.css"
+  },
+  {
+    "revision": "e8d9c01f0de6fc249276",
+    "url": "/white-elephant/static/css/main.87cd0072.chunk.css"
+  },
+  {
+    "revision": "b9e097888d57571795f3",
+    "url": "/white-elephant/static/js/2.d0b76a33.chunk.js"
+  },
+  {
+    "revision": "e8d9c01f0de6fc249276",
+    "url": "/white-elephant/static/js/main.748dc4ad.chunk.js"
+  },
+  {
+    "revision": "a34af9bd8d6753663d30",
+    "url": "/white-elephant/static/js/runtime-main.293b8503.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/white-elephant/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/white-elephant/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/white-elephant/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/white-elephant/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/white-elephant/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
