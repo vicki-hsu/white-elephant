f68ff346254cfea5ff9e939f2ba78371ded514fd
Vicki Hsu <vicki.hsu@noaa.gov>
2019-12-05 21:23:35 -0700

    Add rules of the road

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
 import './App.scss'
 
 const img_success = "https://media.giphy.com/media/gixQfE7XzZfpe/giphy.gif"
@@ -58,7 +65,7 @@ class Game extends React.Component {
       value: "Ready",
       number_of_heads: 0,
       number_of_tails: 0,
-      img_display: "https://webstockreview.net/images600_/hello-clipart-15.png"
+      img_display: "https://media.giphy.com/media/uRD0V1GreQuru/giphy.gif"
     }
   }
 
@@ -126,6 +133,53 @@ class Game extends React.Component {
             <span className={`h4 center-block text-center`} style={{ marginBottom: 25 }}>{"Number of Safe Gifts: " + this.state.number_of_tails}</span>
           </div>
         </div>
+
+        <div className="instructions">
+        <Accordion style={{width: '100%'}} allowZeroExpanded={true}>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        The Probabilistic White Elephant: Rules of the Road
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p><b>Starting with Player #1, they either: </b></p>
+                    <p><b>a) Pick from The Pool of gifts and opens, or </b></p>
+                    <p><b>b) Challenge to steal a non-stolen gift from another Player.</b></p>  
+                    <p><b>(Note: with the first person, their only option is (a), but starting with #2 and beyond, both (a) and (b) are valid options.)</b></p>
+
+                    <p><b>If the Player challenges, then the person with the gift can either freely give it to the Player, or can enter into the challenge, in which case the success of the steal is determined by a coin flip.  If the gift is successfully stolen, then that gift is labeled "stolen" for that round (i.e., it can no longer be challenged for that round). </b></p>
+
+                    <p><b>If an unsuccessful challenge occurs, then the Player must choose to challenge another gift or pick from The Pool. </b></p>
+
+                    <p><b>If a successful challenge occurs, then the person who just gave up the gift must now choose either option (a) or (b).  Note that a gift can only be *stolen* once per round, but a key point is that it can be *challenged* multiple times per round. </b></p>
+
+                    <p><b>The round ends once a gift is selected from The Pool, and the game ends at the end of the last round.</b></p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        Who is the Ultimate Winner?
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>
+                      <span 
+                        className={`center-block text-center`}
+                        style={{ marginBottom: 25, fontSize: 32, fontFamily: "Courier New", fontWeight: "bolder" }}>{"EVERYBODY! HAPPY HOLIDAYS!!!"}
+                      </span>
+                      <img 
+                        src="https://media.giphy.com/media/9w475hDWEPVlu/giphy.gif"
+                        width={ 200 }
+                        height={ 200 }
+                        className="center-block text-center"
+                      />
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
+        </div>
           
         </div>
       </div>
@@ -136,4 +190,3 @@ class Game extends React.Component {
 }
 
 export default Game;
-            // <span className={`h1 center-block text-center`} style={{ marginBottom: 25 }}>{this.state.value}</span>
